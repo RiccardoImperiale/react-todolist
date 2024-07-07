b0acbc0a34f32fd88815e35bdd80a66e5ef7ea60
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-07-07 10:33:37 +0200

    add animation when tasks switching position

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Just Todo It.
 ## (My first attempt at learning React) 
-⚙️ React.js + Local Storage 🤝 [Just Try It](https://boolflix-streaming.netlify.app/)
+⚙️ React.js + Local Storage + Framer Motion 🤝 [Just Try It](https://boolflix-streaming.netlify.app/)
 
 ![alt text](./public/screen-1.png)
 ![alt text](./public/screen-2.png)
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,6 +1,8 @@
 import Task from "./Task"
 import './todolist.css'
 
+import { motion, AnimatePresence } from "framer-motion";
+
 export default function TodoList({ todos, setTodos }) {
     let completedTasks = todos.filter(todo => todo.complete === true).length
     const sortedTasks = todos.slice().sort((a, b) => Number(a.complete) - Number(b.complete))
@@ -16,14 +18,28 @@ export default function TodoList({ todos, setTodos }) {
                             <div className="pill">{completedTasks}</div>
                         </h3>
                     </div>
-                    <div className="task_list">
-                        {sortedTasks.map(item => (
-                            <Task key={item.id} item={item} todos={todos} setTodos={setTodos} />
-                        ))}
-                        <div className="todo_it">
-                            <img width={180} src="./logo-text.png" alt="" />
-                        </div>
-                    </div>
+                    <AnimatePresence>
+                        <motion.div
+                            className="task_list"
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            exit={{ opacity: 0 }}
+                            transition={{ duration: 0.5 }}
+                        >
+                            {sortedTasks.map(item => (
+                                <motion.div
+                                    key={item.id}
+                                    layout
+                                    transition={{ duration: 0.3, type: "spring", stiffness: 500, damping: 30 }}
+                                >
+                                    <Task key={item.id} item={item} todos={todos} setTodos={setTodos} />
+                                </motion.div>
+                            ))}
+                            <div className="todo_it">
+                                <img width={180} src="./logo-text.png" alt="" />
+                            </div>
+                        </motion.div>
+                    </AnimatePresence>
                 </div>
             </div>
         </div>
